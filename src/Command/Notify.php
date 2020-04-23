0f436f9bae1b39d477687f7d06e499c73b4675d1
iggyster <iggyster@users.noreply.github.com>
2020-04-23 12:51:49 +0300

    Update notify command functionality
    An optional parameter has been added with which you can specify the country the weather data of which we want to extract.
    
    Also, a method has been added for simple logging of the command execution process (the log file is ignored, but not the folder).
    
    A functionality has been added that, based on the arguments of the command, creates a message about the weather data and sends it with the corresponding notifier.

--- src/Command/Notify.php
+++ src/Command/Notify.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Command;
 
+use App\Factory\MessageFactory;
+use App\Factory\NotificationStrategyFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Notify extends Command
@@ -20,8 +25,9 @@ class Notify extends Command
     protected function configure()
     {
         $this->setDescription('Send notification');
-        $this->addArgument('type', InputArgument::REQUIRED, '', 'sms');
-        $this->addArgument('to', InputArgument::REQUIRED, '', null);
+        $this->addArgument('type', InputArgument::REQUIRED, 'Set the notification type that you want ot send: sms');
+        $this->addArgument('to', InputArgument::REQUIRED, 'Define receiver address according to the type: phone number (for sms)');
+        $this->addOption('country', 'c', InputOption::VALUE_OPTIONAL, 'A country for which the weather data should be fetched', 'Thessaloniki');
     }
 
     /**
@@ -31,8 +37,32 @@ class Notify extends Command
     {
         $type = $input->getArgument('type');
         $to = $input->getArgument('to');
+        $country = $input->getOption('country');
 
-        $strategy = NotificationStrategyFactory::create($type);
-        $strategy->notify($to);
+        $this->log('Try to send '.$type.' notification to the '.$to);
+
+        $factory = NotificationStrategyFactory::create();
+        $strategy = $factory->createStrategy($type);
+
+        $factory = MessageFactory::create();
+        $message = $factory->createMessageForCountry($country);
+
+        $message->setSender('Weather');
+        $message->setReceiver($to);
+        $strategy->notify($message);
+
+        $this->log('Notification sent successfully');
+
+        return 1;
+    }
+
+    /**
+     * @param string $message
+     */
+    private function log(string $message): void
+    {
+        $path = __DIR__ . '/../../var/log/notifications.log';
+
+        file_put_contents($path, date('Y-m-d H:i:s').' '.$message.PHP_EOL, FILE_APPEND);
     }
 }
